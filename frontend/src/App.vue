ef6d62ea8dcb20e1bf6e7dbf5cf7ece8917b141e
alexandershevtsov <as@digitarium.io>
2025-07-28 22:34:16 +0300

    Initial commit: Vue + TS + Tailwind + FastAPI

---
+++ frontend/src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { onMounted, ref } from 'vue'
+const message = ref('')
+
+onMounted(async () => {
+  const res = await fetch('http://localhost:8000/')
+  const data = await res.json()
+  message.value = data.message
+})
+</script>
+
+<template>
+  <div class="p-4 text-xl font-bold">
+    {{ message }}
+  </div>
+</template>
