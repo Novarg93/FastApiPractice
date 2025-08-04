8fcc287289cdf13f144349c2ae0d5bbdef922cf3
alexandershevtsov <as@digitarium.io>
2025-08-04 17:59:41 +0300

    123

--- backend/app/models/users.py
+++ backend/app/models/users.py
@@ -1,8 +1,8 @@
-from sqlalchemy import Column, Integer, String, Column
+from sqlalchemy import Column, Integer, String
 from app.database.session import Base
 
 class User(Base):
-    __table__ = "users"
+    __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String, unique=True, index=True, nullable=False)
