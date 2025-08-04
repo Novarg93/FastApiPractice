d473f0667ba995f585b550e855ecbbc29e5f5b5d
Abyss <alphaguard1an@mail.ru>
2025-08-04 21:32:18 +0600

    updt

--- backend/app/models/users.py
+++ backend/app/models/users.py
@@ -2,8 +2,8 @@ from sqlalchemy import Column, Integer, String, Column
 from app.database.session import Base
 
 class User(Base):
-    __table__ = "users"
-
+    __tablename__ = "users"
     id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
     email = Column(String, unique=True, index=True, nullable=False)
     hashed_password = Column(String, nullable=False)
