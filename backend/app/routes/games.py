b5afa9040cac60d3225383de463964c91901812c
Abyss <alphaguard1an@mail.ru>
2025-08-19 22:32:35 +0600

    run.py

--- backend/app/routes/games.py
+++ backend/app/routes/games.py
@@ -1,14 +1,17 @@
 # app/routes/games.py
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, Query
 from sqlalchemy.orm import Session
 from app.database.session import get_db
-from app.models import Game
+from app.models import Game, Item
 from app.schemas.games import GameRead
+from app.schemas.items import ItemRead
 
 router = APIRouter(prefix="/games", tags=["games"])
 
 @router.get("/by-slug/{slug}", response_model=GameRead)
 def get_game_by_slug(slug: str, db: Session = Depends(get_db)):
+
+
     game = db.query(Game).filter(Game.slug == slug).first()
     if not game:
         raise HTTPException(status_code=404, detail="Game not found")
@@ -18,3 +21,22 @@ def get_game_by_slug(slug: str, db: Session = Depends(get_db)):
 @router.get("/", response_model=list[GameRead])
 def list_games(db: Session = Depends(get_db)):
     return db.query(Game).all()
+
+# @router.get("/by-slug/{slug}/items", response_model=list[ItemRead])
+# def get_items_by_game(
+#         slug: str,
+#         q: str | None = Query(None, description="Search items by name"),
+#         db: Session = Depends(get_db),
+# ):
+#         if slug == "all":
+#             query = db.query(Item)
+#         else:
+#             game = db.query(Game).filter(Game.slug == slug).first()
+#             if not game:
+#                 raise HTTPException(status_code=404, detail="Game not found")
+#             query = db.query(Item).filter(Item.game_id == game.id)
+#
+#         if q:
+#             query = query.filter(Item.name.ilike(f"%{q}%"))
+#
+#         return query.all()
